877cdc55dacc6824ae3aee9445b45ecc5de53cde
Florian Guldner <florian.guldner@googlemail.com>
2024-10-24 17:03:54 +0000

    add secret_key and database_url1

--- codestar/settings.py
+++ codestar/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%h)g-mo49vx=&pp!^lry6#9&5j4$ewwjnznsr=)^4doicgb3-*'
+SECRET_KEY = os.environ.get('django-insecure-%h)g-mo49vx=&pp!^lry6#9&5j4$ewwjnznsr=)^4doicgb3-*')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
